7b62dd1b1da3afed418cd11f5141232288e2ebdc
csbrotherslabs <95954737+csbrotherslabs@users.noreply.github.com>
2026-09-10 22:14:09 -0500

    Add create and edit store routes

--- sellers_app/urls.py
+++ sellers_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('products/<int:product_id>/edit/', views.edit_product, name='edit_product'),
     path('orders/', views.order_list, name='seller_orders'),
     path('orders/<int:order_id>/', views.order_detail, name='seller_order_detail'),
-    path('store/setup/', views.store_setup, name='seller_store_setup'),
+    path('stores/add/', views.store_setup, name='seller_store_setup'),
+    path('stores/<int:store_id>/edit/', views.store_setup, name='seller_store_edit'),
 ]
