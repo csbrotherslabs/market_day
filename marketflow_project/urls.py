e8c1cf2009a2f21e4462f562f80b7a28f6db1655
csbrotherslabs <95954737+csbrotherslabs@users.noreply.github.com>
2026-09-10 21:21:23 -0500

    Add password reset routes

--- marketflow_project/urls.py
+++ marketflow_project/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path, reverse_lazy
 from core_app import views as core_views
 
 urlpatterns = [
@@ -11,6 +12,38 @@ urlpatterns = [
     path('accounts/register/', core_views.register_view, name='register'),
     path('accounts/logout/', core_views.logout_view, name='logout'),
     path('accounts/profile/', core_views.profile_view, name='profile'),
+    path(
+        'accounts/password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='core_app/password_reset_form.html',
+            email_template_name='core_app/password_reset_email.html',
+            subject_template_name='core_app/password_reset_subject.txt',
+            success_url=reverse_lazy('password_reset_done'),
+        ),
+        name='password_reset',
+    ),
+    path(
+        'accounts/password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='core_app/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='core_app/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete'),
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'accounts/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='core_app/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
+    ),
     path('', include('core_app.urls')),
     path('buyers/', include('buyers_app.urls')),
     path('sellers/', include('sellers_app.urls')),
@@ -21,4 +54,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
