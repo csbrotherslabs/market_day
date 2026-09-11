4cea677858f5cde2fd7b72e0780e53d8383aec78
csbrotherslabs <95954737+csbrotherslabs@users.noreply.github.com>
2026-09-10 21:26:24 -0500

    Use temporary direct password reset in development

--- marketflow_project/urls.py
+++ marketflow_project/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import include, path, reverse_lazy
+from django.urls import include, path
 from core_app import views as core_views
 
 urlpatterns = [
@@ -12,38 +11,7 @@ urlpatterns = [
     path('accounts/register/', core_views.register_view, name='register'),
     path('accounts/logout/', core_views.logout_view, name='logout'),
     path('accounts/profile/', core_views.profile_view, name='profile'),
-    path(
-        'accounts/password-reset/',
-        auth_views.PasswordResetView.as_view(
-            template_name='core_app/password_reset_form.html',
-            email_template_name='core_app/password_reset_email.html',
-            subject_template_name='core_app/password_reset_subject.txt',
-            success_url=reverse_lazy('password_reset_done'),
-        ),
-        name='password_reset',
-    ),
-    path(
-        'accounts/password-reset/done/',
-        auth_views.PasswordResetDoneView.as_view(
-            template_name='core_app/password_reset_done.html',
-        ),
-        name='password_reset_done',
-    ),
-    path(
-        'accounts/reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='core_app/password_reset_confirm.html',
-            success_url=reverse_lazy('password_reset_complete'),
-        ),
-        name='password_reset_confirm',
-    ),
-    path(
-        'accounts/reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name='core_app/password_reset_complete.html',
-        ),
-        name='password_reset_complete',
-    ),
+    path('accounts/password-reset/', core_views.temporary_password_reset, name='password_reset'),
     path('', include('core_app.urls')),
     path('buyers/', include('buyers_app.urls')),
     path('sellers/', include('sellers_app.urls')),
