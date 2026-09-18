91a0ea064c54e2eac0c56259ef20c9850327650d
csbrotherslabs <95954737+csbrotherslabs@users.noreply.github.com>
2026-09-18 01:57:43 -0500

    Fix missing Category import in marketplace views

--- core_app/views.py
+++ core_app/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from .models import (
+    Category,
     DeliveryAssignment,
     Dispute,
     DriverProfile,
